e887a99b2d797c2364a6a125311c8f26b87940a6
sarfaroj <sarfaroj04@gmail.com>
2023-04-10 16:02:15 +0530

    path changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('src/app/components/home/home.module').then(m => m.HomeModule) },
 ];
 
